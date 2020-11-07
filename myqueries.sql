b628b84510608bfd189610c0542f23b5bbe8900a
Johannes Zarre <zarre.johannes@gmail.com>
2020-11-07 16:41:48 +0100

    added 10 queries

--- myqueries.sql
+++ myqueries.sql
@@ -58,7 +58,8 @@ WHERE package.delivery_time IS NULL AND processing.type = 'DELIVERY_END'
 
 /*6. Report:
    
-Show Full History of each Package including time, employee, location, and Recipient */
+Show Full History of each Package including time, employee, location, and Recipient (BONUS:Use JOINS in your SQL queries that connect more than 3 tables. )*/
+/*My customers can be recipients aswell so i used the customer table 2 times in this query 1 time as sender and 1 time as recipient */
 
 SELECT processing.id, package.package_id, package.tracking_number, processing.type, processing.time AS Time, employee.first_name AS EmployeeFirstName, employee.last_name AS EmployeeLastName, location.name AS LocationName, location.street LocationStreet, location.street_nr AS StrNr, location.zip_code AS ZIPCODE, location.city AS CITY, recipient.first_name AS RecipientFirstName, recipient.last_name AS RecipientLastName
 from package
@@ -69,4 +70,37 @@ INNER JOIN employee ON employee_id = created_by
 LEFT JOIN customer AS Recipient ON recipient_id = Recipient.customer_id
 ----------------------------------------------------------------------------------------------------------------------
 
+/*7. Report:
+   
+Show all packages and theyre tracking number which where handover to customer in a store */
 
+SELECT package.package_id, package.tracking_number, location.name AS LocationName, location.street LocationStreet, location.street_nr AS StrNr, location.zip_code AS ZIPCODE, location.city AS CITY
+from package
+INNER JOIN processing ON package_id = p_id
+LEFT JOIN location ON location_id = l_id
+WHERE processing.location_id = 2 AND processing.type = 'HANDOVER_AT_STORE'
+
+/*8. Report:
+   
+Show all packages and theyre Sender which were sent between a date */
+
+SELECT package_id, tracking_number, customer.first_name AS SenderFirstName, customer.last_name AS SenderLastName
+from package
+INNER JOIN customer on customer.customer_id = package.sender_id
+WHERE package.deposit_time BETWEEN '2020-11-02' AND '2020-11-11' --change date for desired query--
+
+/*9. Report:
+   
+Show all packages which were delivered and the date */
+
+SELECT package.package_id, package.tracking_number, package.delivery_time
+from package
+WHERE package.delivery_time IS NOT NULL
+
+/*10. Report:
+   
+Show all packages which picked up at sender location */
+SELECT processing.id, package.package_id, package.tracking_number
+from package
+INNER JOIN processing ON processing.p_id = package.package_id
+WHERE processing.type = 'PICKUP_AT_SENDER_LOCATION'
